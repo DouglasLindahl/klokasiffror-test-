5a6e7103d90df276547b72315c706ce7aadb50c9
DouglasLindahl <douglas.lindahl@gmail.com>
2026-02-22 13:26:59 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -237,9 +237,13 @@ export default function Home() {
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 sm:py-32">
           <div className="text-center">
-            <h1 className="text-3xl sm:text-4xl lg:text-5xl mb-6">
-              KLOKA SIFFROR AB
-            </h1>
+            <div className="flex justify-center mb-8 pb-8">
+              <img
+                src="test2.png"
+                alt="Kloka Siffror"
+                className="h-24 sm:h-24 lg:h-58 w-auto opacity-95"
+              />
+            </div>
             <h1 className="text-5xl sm:text-6xl lg:text-7xl mb-6">
               Redovisning som gör skillnad.
             </h1>
